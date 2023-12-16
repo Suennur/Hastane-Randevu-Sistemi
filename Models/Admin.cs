da9645d05ec0c8880afd05dd4b3887714ddaff54
Suennur <83010778+Suennur@users.noreply.github.com>
2023-12-16 13:13:43 +0300

    Update Admin.cs

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -2,12 +2,20 @@
 {
     public class Admin
     {
+        [Key]
         public int AdminId { get; set; }
-
+        
+        [MinLength(2, ErrorMessage = "Minimum 2 karakter")]
+        [MaxLength(50,ErrorMessage ="Maksimum 50 karakter")]
+        [Required(ErrorMessage ="Bu alanın doldurulması zorunludur")]
+        [Display(Name ="Kullanıcı Adı")]
         public string AdminName { get; set; }
-
+        
+        [MaxLength(20, ErrorMessage = "Maksimum 20 karakter")]
+        [MinLength(4, ErrorMessage = "Minimum 4 karakter")]
+        [Required(ErrorMessage = "Bu alanın doldurulması zorunludur")]
+        [Display(Name = "Kullanıcı Şifre")]
         public string AdminPassword { get; set; }
-
 
     }
 }
