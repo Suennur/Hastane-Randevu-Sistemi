9ea972d147858f126f07f8aa5cbb7ab476fb3b74
Suennur <83010778+Suennur@users.noreply.github.com>
2023-12-16 13:15:14 +0300

    Update Doctor.cs

--- Models/Doctor.cs
+++ Models/Doctor.cs
@@ -2,14 +2,35 @@
 {
     public class Doctor
     {
+        [Key]
         public int DoctorId { get; set; }
 
+
+        [MaxLength(25, ErrorMessage = "Maksimum 25 karakter")]
+        [Required(ErrorMessage ="Bu alanın doldurulması zorunludur.")]
+        [Display(Name = "AD")]
         public string DoctorFName { get; set;}
 
+
+        [MaxLength(20, ErrorMessage = "Maksimum 20 karakter")]
+        [Required(ErrorMessage = "Bu alanın doldurulması zorunludur.")]
+        [Display(Name = "SOYAD")]
         public string DoctorLName { get; set;}
 
-        public DateTime WorkTime { get; set;}
 
+        [Required(ErrorMessage = "Bu alanın doldurulması zorunludur.")]
+        [Display(Name = "ÇALIŞMA GÜNLERİ")]
+        public DayOfWeek WorkDay { get; set;}
+
+        [Required(ErrorMessage = "Bu alanın doldurulması zorunludur.")]
+        [Display(Name = "ÇALIŞMA SAATLERİ")]
+        public DateTime DateTime { get; set;} 
+
+
+        public ICollection<Specialization> Specializations { get; set; }
+
+
+        public ICollection<Appointment> Appointments { get; set; }
 
     }
 }
