1f68108abb932d9a0584e87e49751bf3a2961e4f
Suennur <83010778+Suennur@users.noreply.github.com>
2023-12-18 11:58:54 +0300

    Update Policlinic.cs

---
+++ Models/Policlinic.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+namespace Hastane.Models
+{
+    public class Policlinic
+    {
+        [Key]
+        public int PolicID { get; set; }
+
+        public string PolicName { get; set; }
+    }
+}
