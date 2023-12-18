3af3bd2f3fc15eae67a0b5bfe62f8b3a05a750d7
Suennur <83010778+Suennur@users.noreply.github.com>
2023-12-18 11:57:20 +0300

    Update Doctor.cs

--- Models/Doctor.cs
+++ Models/Doctor.cs
@@ -1,16 +1,19 @@
-﻿namespace Hastane.Models
+using System.ComponentModel.DataAnnotations;
+namespace Hastane.Models
 {
     public class Doctor
     {
         [Key]
         public int DoctorId { get; set; }
 
+        
         [MinLength(2, ErrorMessage = "Minimum 2 karakter")]
         [MaxLength(25, ErrorMessage = "Maksimum 25 karakter")]
         [Required(ErrorMessage ="Bu alanın doldurulması zorunludur.")]
         [Display(Name = "AD")]
         public string DoctorFName { get; set;}
 
+        
         [MinLength(2, ErrorMessage = "Minimum 2 karakter")]
         [MaxLength(20, ErrorMessage = "Maksimum 20 karakter")]
         [Required(ErrorMessage = "Bu alanın doldurulması zorunludur.")]
@@ -22,14 +25,11 @@
         [Display(Name = "ÇALIŞMA GÜNLERİ")]
         public DayOfWeek WorkDay { get; set;}
 
-        [Required(ErrorMessage = "Bu alanın doldurulması zorunludur.")]
-        [Display(Name = "ÇALIŞMA SAATLERİ")]
-        public DateTime DateTime { get; set;} 
+        
+        Policlinic? policlinic { get; set; }
+        public int PolicID { get; set; }
 
-
-        public ICollection<Specialization> Specializations { get; set; }
-
-
+        
         public ICollection<Appointment> Appointments { get; set; }
 
     }
