57a61b01f4ae1cb6b3eb689f77f4036aafed7a6d
Suennur Altaş <83010778+Suennur@users.noreply.github.com>
2023-12-25 17:18:16 +0300

    Add files via upload

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Has.Models;
+
+namespace Has.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly HasDataContext _context;
+
+        public AppointmentController(HasDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Appointment
+        public async Task<IActionResult> Index()
+        {
+              return _context.Appointments != null ? 
+                          View(await _context.Appointments.ToListAsync()) :
+                          Problem("Entity set 'HasDataContext.Appointments'  is null.");
+        }
+
+        // GET: Appointment/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Appointments == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments
+                .FirstOrDefaultAsync(m => m.AppointmentID == id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return View(appointment);
+        }
+
+        // GET: Appointment/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Appointment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AppointmentID,AppointmentTime,DoctorID,PatientID")] Appointment appointment)
+        {
+            if (ModelState.IsValid || true)
+            {
+                _context.Add(appointment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(appointment);
+        }
+
+        // GET: Appointment/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Appointments == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            return View(appointment);
+        }
+
+        // POST: Appointment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AppointmentID,AppointmentTime,DoctorID,PatientID")] Appointment appointment)
+        {
+            if (id != appointment.AppointmentID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid || true)
+            {
+                try
+                {
+                    _context.Update(appointment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AppointmentExists(appointment.AppointmentID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(appointment);
+        }
+
+        // GET: Appointment/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Appointments == null)
+            {
+                return NotFound();
+            }
+
+            var appointment = await _context.Appointments
+                .FirstOrDefaultAsync(m => m.AppointmentID == id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return View(appointment);
+        }
+
+        // POST: Appointment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Appointments == null)
+            {
+                return Problem("Entity set 'HasDataContext.Appointments'  is null.");
+            }
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment != null)
+            {
+                _context.Appointments.Remove(appointment);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AppointmentExists(int id)
+        {
+          return (_context.Appointments?.Any(e => e.AppointmentID == id)).GetValueOrDefault();
+        }
+    }
+}
