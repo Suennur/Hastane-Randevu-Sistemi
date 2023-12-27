c8e42a59bf742bdedf966818d72803b5e728deb4
Suennur Altaş <83010778+Suennur@users.noreply.github.com>
2023-12-27 13:45:51 +0300

    Add files via upload

---
+++ Controllers/HasDataContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Has.Models
+{
+    public partial class HasDataContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Policlinic> Policlinics { get; set; }
+
+        public HasDataContext(DbContextOptions<HasDataContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=xHasData;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Admin>(entity =>
+            {
+                entity.Property(e => e.AdminName)
+                    .HasMaxLength(500)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.AdminPassword)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+            });
+            
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+        public HasDataContext()
+        {
+        }
+    }
+}
