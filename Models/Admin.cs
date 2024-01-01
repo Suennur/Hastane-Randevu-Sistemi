8e6afc0387900dec74bdde152dd5e2a83c6e5686
Suennur Altaş <83010778+Suennur@users.noreply.github.com>
2024-01-01 20:16:40 +0300

    Update Admin.cs

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -1,21 +1,9 @@
-﻿namespace Hastane.Models
+namespace Hastane.Models
 {
     public class Admin
     {
-        [Key]
         public int AdminId { get; set; }
-        
-        [MinLength(2, ErrorMessage = "Minimum 2 karakter")]
-        [MaxLength(50,ErrorMessage ="Maksimum 50 karakter")]
-        [Required(ErrorMessage ="Bu alanın doldurulması zorunludur")]
-        [Display(Name ="Kullanıcı Adı")]
-        public string AdminName { get; set; }
-        
-        [MaxLength(20, ErrorMessage = "Maksimum 20 karakter")]
-        [MinLength(4, ErrorMessage = "Minimum 4 karakter")]
-        [Required(ErrorMessage = "Bu alanın doldurulması zorunludur")]
-        [Display(Name = "Kullanıcı Şifre")]
-        public string AdminPassword { get; set; }
-
+        public string? AdminName { get; set; }
+        public string? AdminPassword { get; set; }
     }
 }
