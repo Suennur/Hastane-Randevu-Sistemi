afe2f0c77263707cb5b15eb55bcc9b2dee988df8
Suennur Altaş <83010778+Suennur@users.noreply.github.com>
2024-01-01 20:20:02 +0300

    Update Doctor.cs

--- Models/Doctor.cs
+++ Models/Doctor.cs
@@ -1,36 +1,30 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Reflection.Metadata.BlobBuilder;
+
 namespace Hastane.Models
 {
     public class Doctor
     {
         [Key]
-        public int DoctorId { get; set; }
+        public int doctorId { get; set; }
 
-        
-        [MinLength(2, ErrorMessage = "Minimum 2 karakter")]
-        [MaxLength(25, ErrorMessage = "Maksimum 25 karakter")]
-        [Required(ErrorMessage ="Bu alanın doldurulması zorunludur.")]
-        [Display(Name = "AD")]
-        public string DoctorFName { get; set;}
+        [Required(ErrorMessage ="This field required")]
+        [Display(Name ="Doctor")]
+        public string? doctorName{ get; set; }
 
-        
-        [MinLength(2, ErrorMessage = "Minimum 2 karakter")]
-        [MaxLength(20, ErrorMessage = "Maksimum 20 karakter")]
-        [Required(ErrorMessage = "Bu alanın doldurulması zorunludur.")]
-        [Display(Name = "SOYAD")]
-        public string DoctorLName { get; set;}
+        [Required(ErrorMessage = "This field required")]
+        [Display(Name = "Policlinic")]
+        public int polic { get; set; }
+
+        [Required(ErrorMessage = "This field required")]
+        [Display(Name = "Work Time")]
+        public DateTime workTime { get; set; }
 
 
-        [Required(ErrorMessage = "Bu alanın doldurulması zorunludur.")]
-        [Display(Name = "ÇALIŞMA GÜNLERİ")]
-        public DayOfWeek WorkDay { get; set;}
+        [ForeignKey("polic")]
+        public Policlinic policlinic { get; set; }
 
-        [ForeignKey("PolicID")]
-        Policlinic? policlinic { get; set; }
-        public int PolicID { get; set; }
-
-        
-        public ICollection<Appointment> Appointments { get; set; }
-
+        public ICollection<Appointment> Appointments{ get; set; }
     }
 }
