6a7118e493e1c3eb8719bca4e314beadede08c62
Suennur Altaş <83010778+Suennur@users.noreply.github.com>
2024-01-01 20:21:44 +0300

    Update Policlinic.cs

--- Models/Policlinic.cs
+++ Models/Policlinic.cs
@@ -1,11 +1,15 @@
 using System.ComponentModel.DataAnnotations;
+
 namespace Hastane.Models
 {
     public class Policlinic
     {
         [Key]
-        public int PolicID { get; set; }
+        public int PolicId{ get; set; }
 
-        public string PolicName { get; set; }
+        [Required]
+        [Display(Name ="Policlinic")]
+        public string? PolicName { get; set;}
+
     }
-} 
+}
