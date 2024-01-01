e75cb2015a4982f6968f8203bfa614a48e8d0399
Suennur Altaş <83010778+Suennur@users.noreply.github.com>
2024-01-01 20:17:26 +0300

    Update PatientController.cs

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -1,60 +1,111 @@
-﻿using Hastane.Migrations;
 using Hastane.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data.Entity;
+using System.Numerics;
 
 namespace Hastane.Controllers
 {
     public class PatientController : Controller
     {
-        [HttpGet]
-        public IActionResult Login()
+
+        HospitalDataContext _context = new HospitalDataContext();
+        public IActionResult Index()
         {
-            List<Patient> patientlist = new List<Patient>();
+            if (HttpContext.Session.GetString("SessionUser") is null)
+            {
+                TempData["error"] = "You are not authorized to access this page.";
+                return RedirectToAction("Login");
+            }
+            
             return View();
         }
-        [HttpPost]
-        public IActionResult Login(List<Patient> patients)
-        {
-            HastaneDataContext _context = new HastaneDataContext();
-            foreach (Patient patient in patients)
-            {
-                var status = _context.Patients.Where(x => x.PatientTC == patient.PatientTC && x.PatientPassword == patient.PatientPassword);
-                if (status == null)
-                {
-                    ViewBag.Status = 0;
-                }
-                else
-                {
-                    HttpContext.Session.SetString("SessionUser", patient.PatientTC);
-                    var cookieOpt = new CookieOptions
-                    {
-                        Expires = DateTime.Now.AddSeconds(5)
-                    };
 
-                    return RedirectToAction("Index", "Patient");
-                }
-                
-            }
-            return View(patients);
+        public IActionResult Signin()
+        {
             
+            return View();
         }
+
+        [HttpPost]
+        public IActionResult Signin([Bind("PatientName,PatientSurname,tc,PatientPassword")] Patient p)
+        {
+            _context.Patients.Add(p);
+            _context.SaveChanges();
+
+            return RedirectToAction("Login");
+
+
+        }
+
+        public IActionResult Login()
+        {
+            Patient patient = new Patient();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Patient p)
+        {
+            var status = _context.Patients.FirstOrDefault(x => x.tc == p.tc && x.PatientPassword == p.PatientPassword);
+            
+            if (status == null)
+            {
+                ViewBag.Status = 0;
+            }
+            else
+            {
+                HttpContext.Session.SetString("SessionUser", p.tc);
+                var cookieOpt = new CookieOptions
+                {
+                    Expires = DateTime.Now.AddMinutes(5),
+                };
+
+                return RedirectToAction("Index", "Patient");
+
+            }
+            return View(p);
+
+        }
+
         public IActionResult Logout()
         {
             HttpContext.Session.Clear();
             return RedirectToAction("Login", "Patient");
         }
-        public IActionResult Index()
+        
+        public async Task<IActionResult> MakeAppointment()
         {
             if (HttpContext.Session.GetString("SessionUser") is null)
             {
-                TempData["hata"] = "Bu sayfaya erişim yetkiniz yok.";
+                TempData["error"] = "You are not authorized to access this page.";
                 return RedirectToAction("Login");
             }
+            
+            ViewData["DoctorList"] = new SelectList(_context.Doctors, "doctorId", "doctorName");
             return View();
         }
-        public IActionResult Signin()
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MakeAppointment([Bind("AppoId, DoctorId")]Appointment appointment)
         {
-            return View();
+
+            string Tc = HttpContext.Session.GetString("SessionUser");
+            Patient patient = _context.Patients.FirstOrDefault(p => p.tc == Tc);
+            appointment.PatientId = patient.PatientId;
+            if (ModelState.IsValid || true)
+            {
+                _context.Add(appointment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            //ViewData["PoliclinicList"] = new SelectList(_context.Policlinics, "PolicId", "PolicName");
+            ViewData["DoctorList"] = new SelectList(_context.Doctors, "doctorId", "doctorName" );
+            ViewData["WorkDayList"] = new SelectList(_context.Doctors, "doctorId", "workTime");
+            //ViewData["PatientList"] = new SelectList(_context.Patients, "PatientId", "PatientName");
+            return View(appointment);
         }
+
     }
 }
